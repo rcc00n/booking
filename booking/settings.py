b8f6ddec8b62791c85bdbd64adc2bdc043d556c9
rcc00n <vadrud2016@gmail.com>
2025-10-08 03:04:43 -0600

    db fix

--- booking/settings.py
+++ booking/settings.py
@@ -17,7 +17,6 @@ import environ
 import os
 import os
 import environ
-import dj_database_url
 
 env = environ.Env()
 environ.Env.read_env()
@@ -108,12 +107,11 @@ WSGI_APPLICATION = 'booking.wsgi.application'
 #         'PORT': '5432',
 #     }
 # }
-
+#
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=False,   # True только если внешний managed PG требует SSL
+    "default": env.db(
+        "DATABASE_URL",
+        default="postgres://postgres@localhost:5432/postgres",
     )
 }
 
