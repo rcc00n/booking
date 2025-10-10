b29f283e606ed64485280cc437d06c578d969dc2
Oleksandr Lisnytskyi <oleksandrl1009@gmail.com>
2025-10-09 23:36:17 -0600

    fixed db

--- booking/settings.py
+++ booking/settings.py
@@ -95,16 +95,16 @@ WSGI_APPLICATION = 'booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Malva', # Change to your local DB name
-        'USER': 'postgres', # Change to your local user name
-        'PASSWORD': 'admin', # Change to your local user password
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Malva', # Change to your local DB name
+#         'USER': 'postgres', # Change to your local user name
+#         'PASSWORD': 'admin', # Change to your local user password
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 # DATABASES = {
 #     "default": env.db(
@@ -114,13 +114,13 @@ DATABASES = {
 # }
 
 
-# DATABASES = {
-#     "default": dj_database_url.parse(
-#         os.environ["DATABASE_URL"],
-#         conn_max_age=600,
-#         ssl_require=False,
-#     )
-# }
+DATABASES = {
+    "default": dj_database_url.parse(
+        os.environ["DATABASE_URL"],
+        conn_max_age=600,
+        ssl_require=False,
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
